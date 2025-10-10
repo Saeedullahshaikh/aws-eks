7297346b7b4e6221f4f335b4182c706f8d7ed51f
Saeedullahshaikh <Saeedushaikh276@gmail.com>
2025-10-10 19:34:03 +0530

    Add files via upload

---
+++ eks.tf
@@ -0,0 +1,42 @@
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name                  = local.name
+  cluster_endpoint_public_access = true
+
+  vpc_id                     = module.vpc.vpc_id
+  subnet_ids                 = module.vpc.private_subnets
+  control_plane_subnet_ids   = module.vpc.intra_subnets
+
+  cluster_addons = {
+    vpc-cni = {
+      most-recent = true
+    }
+    kube-proxy = {
+      most-recent = true
+    }
+    coredns = {
+      most-recent = true
+    }
+  }
+
+  eks_managed_node_group_defaults = {
+    instance_types                          = ["t2.medium"]
+    attach_cluster_primary_security_group   = true
+  }
+
+  eks_managed_node_groups = {
+    tws-cluster-ng = {
+      instance_types = ["t2.medium"]
+      min            = 2
+      max            = 3
+      desired_size   = 2
+      capacity_type  = "SPOT"
+    }
+  }
+
+  tags = {
+    Environment = local.env
+    Terraform   = "true"
+  }
+}
